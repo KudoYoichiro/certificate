5092dbeeeac6cb1ccea2c0e47e8fd1b5304f0711
KudoYoichiro <artifical.wisteria@gmail.com>
2014-08-15 11:24:55 +0900

    change stylesheet for maps index page

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -3,7 +3,7 @@ module MapsHelper
     map = Map.find_by('instrument_id = ? AND engineer_id = ? AND operation_id = ?',
     instrument_id, engineer_id, operation_id)
     
-    result = map.blank? ? "" : map.status.name
+    result = map.blank? || map.status.blank? ? "" : map.status.name
     
   end
 end
